648f4bb89df07d393d3f138c5e892c5fb88b906f
Lukas Piccini <lukaspiccini@hotmail.com>
2019-06-29 22:35:15 -0300

    Restructured project folders

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const barbecue_router = require("./routes/barbecue");
+const attendant_router = require("./routes/attendant");
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use("/api",barbecue_router);
+app.use("/api",attendant_router);
+
+app.listen(3000, () => {
+    console.log('Conectado na porta 3000')
+})
