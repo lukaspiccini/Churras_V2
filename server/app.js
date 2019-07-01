008801f9f94551c15c0a96b5002665af8a62935e
Lukas Piccini <lukaspiccini@hotmail.com>
2019-07-01 20:02:55 -0300

    Adjusting server-side for deploy

--- server/app.js
+++ server/app.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const connection = require("./database/database");
-const barbecue_router = require("./routes/barbecue");
-const attendant_router = require("./routes/attendant");
+var cors = require('cors')
+const connection = require("./src/database/database");
+const barbecue_router = require("./src/routes/barbecue");
+const attendant_router = require("./src/routes/attendant");
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use(cors())
 app.use("/api",barbecue_router);
 app.use("/api",attendant_router);
 
-app.listen(3000, () => {
-    console.log('Conectado na porta 3000')
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Conectado')
 })
